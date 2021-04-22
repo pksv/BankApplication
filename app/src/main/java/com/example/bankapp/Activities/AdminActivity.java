053331e7ae2d20b5eb5c97d1d4a201defe092e3b
pksv <pksv@users.noreply.github.com>
2021-04-22 14:05:25 +0530

    Logout Button

--- app/src/main/java/com/example/bankapp/Activities/AdminActivity.java
+++ app/src/main/java/com/example/bankapp/Activities/AdminActivity.java
@@ -1,8 +1,10 @@
 package com.example.bankapp.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bankapp.R;
 import com.google.android.material.appbar.MaterialToolbar;
@@ -10,13 +12,19 @@ import com.google.android.material.appbar.MaterialToolbar;
 public class AdminActivity extends AppCompatActivity {
 
     private MaterialToolbar toolbar;
+    private ImageView logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
         toolbar = findViewById(R.id.toolbar);
+        logout = findViewById(R.id.logout);
         setSupportActionBar(toolbar);
+        logout.setOnClickListener(v -> {
+            Intent intent = new Intent(AdminActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 
     @Override
--- app/src/main/java/com/example/bankapp/Activities/UserActivity.java
+++ app/src/main/java/com/example/bankapp/Activities/UserActivity.java
@@ -1,8 +1,10 @@
 package com.example.bankapp.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bankapp.R;
 import com.google.android.material.appbar.MaterialToolbar;
@@ -10,18 +12,23 @@ import com.google.android.material.appbar.MaterialToolbar;
 public class UserActivity extends AppCompatActivity {
 
     private MaterialToolbar toolbar;
+    private ImageView logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
         toolbar = findViewById(R.id.toolbar);
+        logout = findViewById(R.id.logout);
         setSupportActionBar(toolbar);
-
+        logout.setOnClickListener(v -> {
+            Intent intent = new Intent(UserActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 
     @Override
     public void onBackPressed() {
-
+        
     }
 }
